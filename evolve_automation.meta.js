fccb6360d52e836b6bbfe9cc45081789303a5e2b
Skye <xorngaming@gmail.com>
2025-05-09 08:30:20 +0100

    THIS TOO

--- evolve_automation.meta.js
+++ evolve_automation.meta.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Evolve Skye
 // @namespace    http://tampermonkey.net/
-// @version      3.3.1.143
+// @version      3.3.1.144
 // @description  try to take over the world!
 // @downloadURL  https://github.com/SkyeAmphi/Evolve-Automation-Skye/raw/refs/heads/master/evolve_automation.user.js
 // @updateURL    https://github.com/SkyeAmphi/Evolve-Automation-Skye/raw/refs/heads/master/evolve_automation.meta.js
