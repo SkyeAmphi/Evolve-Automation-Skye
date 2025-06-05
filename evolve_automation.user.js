24b2743a1b58f7478921ceec0b31e5a0351c8042
Skye <xorngaming@gmail.com>
2025-06-05 05:20:05 +0100

    use CityAction for Warlord city structures
    this will still not work properly until Obliv's PR to fix building id templates is merged into a game update, and may also require some of my other working changes to fully function. Will add as needed

--- evolve_automation.user.js
+++ evolve_automation.user.js
@@ -2792,9 +2792,9 @@
         WastelandDigDemon: new Action("Wasteland Dig Demon Burrow (Warlord)", "portal", "dig_demon", "prtl_wasteland"),
         WastelandTunneler: new Action("Wasteland Tunneler Demon (Warlord)", "portal", "tunneler", "prtl_wasteland"),
         WastelandBrute: new Action("Wasteland Brute Hut (Warlord)", "portal", "brute", "prtl_wasteland", {garrison: true}),
-        WastelandAltar: new Action("Wasteland Sacrificial Altar (Warlord)", "portal", "s_alter", "prtl_wasteland"),
-        WastelandShrine: new Action("Wasteland Shrine (Warlord)", "portal", "shrine", "prtl_wasteland"),
-        WastelandMeditationChamber: new Action("Wasteland Meditation Chamber (Warlord)", "portal", "meditation", "prtl_wasteland"),
+        WastelandAltar: new CityAction("Wasteland Sacrificial Altar (Warlord)", "portal", "s_alter", "prtl_wasteland"),
+        WastelandShrine: new CityAction("Wasteland Shrine (Warlord)", "portal", "shrine", "prtl_wasteland"),
+        WastelandMeditationChamber: new CityAction("Wasteland Meditation Chamber (Warlord)", "portal", "meditation", "prtl_wasteland"),
 
         PitMission: new Action("Pit Mission", "portal", "pit_mission", "prtl_pit"),
         PitAssaultForge: new Action("Pit Assault Forge", "portal", "assault_forge", "prtl_pit"),
