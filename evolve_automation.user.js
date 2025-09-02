ff4653318bc26a2fefaafd10fcb4366eb739b869
Skye <xorngaming@gmail.com>
2025-09-02 01:32:19 -0400

    changing name so it can be an extra script

--- evolve_automation.user.js
+++ evolve_automation.user.js
@@ -1,5 +1,5 @@
 // ==UserScript==
-// @name         Evolve Skye
+// @name         Evolve Skye Kewne
 // @namespace    http://tampermonkey.net/
 // @version      3.3.1.146
 // @description  try to take over the world!
